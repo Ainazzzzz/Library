1d643b25aa8d10cd98f22c5bbcb901fba5663278
beka.jpg <Hikitaprevstavlenie@gmail.com>
2024-11-30 20:43:04 +0600

    v3

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,34 @@ public class Main {
 
 
 
+//        Client[] clients = {
+//                new Client("Анна Смирнова", 100, 25),
+//                new Client("Иван Иванов", 50, 30),
+//                new Client("Елена Кузнецова", 75, 22),
+//                new Client("Олег Васильев", 40, 35),
+//                new Client("Мария Петрова", 90, 28),
+//                new Client("Алексей Сидоров", 60, 45),
+//                new Client("Светлана Орлова", 80, 33),
+//                new Client("Дмитрий Кузьмин", 70, 40),
+//                new Client("Ольга Зайцева", 110, 26),
+//                new Client("Павел Морозов", 30, 50)
+//        };
+
+
+
         Client[] clients = {
-                new Client("Анна Смирнова", 100, 25),
-                new Client("Иван Иванов", 50, 30),
-                new Client("Елена Кузнецова", 75, 22),
-                new Client("Олег Васильев", 40, 35),
-                new Client("Мария Петрова", 90, 28),
-                new Client("Алексей Сидоров", 60, 45),
-                new Client("Светлана Орлова", 80, 33),
-                new Client("Дмитрий Кузьмин", 70, 40),
-                new Client("Ольга Зайцева", 110, 26),
-                new Client("Павел Морозов", 30, 50)
+                new Client("Анна Смирнова", 25, 100),
+                new Client("Иван Иванов", 30, 50),
+                new Client("Елена Кузнецова", 22, 750),
+                new Client("Олег Васильев", 20, 300),
+                new Client("Мария Петрова", 39, 2800),
+                new Client("Алексей Сидоров", 60, 3999),
+                new Client("Светлана Орлова", 20, 8999),
+                new Client("Дмитрий Кузьмин", 33, 788),
+                new Client("Ольга Зайцева", 12, 788),
+                new Client("Павел Морозов", 18, 900)
         };
 
-
-
-//        Client[] clients = {
-//                new Client("Анна Смирнова", 25, 100),
-//                new Client("Иван Иванов", 30, 50),
-//                new Client("Елена Кузнецова", 22, 750),
-//                new Client("Олег Васильев", 20, 300),
-//                new Client("Мария Петрова", 39, 2800),
-//                new Client("Алексей Сидоров", 60, 3999),
-//                new Client("Светлана Орлова", 20, 8999),
-//                new Client("Дмитрий Кузьмин", 33, 788),
-//                new Client("Ольга Зайцева", 12, 788),
-//                new Client("Павел Морозов", 18, 900)
-//        };
-//
 
         Book[] books = {
                 new Book("Война и мир", "Лев Толстой", 1225, "Роман", LocalDate.of(1869, 1, 1), false),
@@ -53,22 +53,22 @@ public class Main {
                 new Book("Старик и море", "Эрнест Хемингуэй", 127, "Рассказ", LocalDate.of(1952, 9, 1), true)
         };
 
-        Bron[] reservations = new Bron[10];
-
-
-        for (int i = 0; i < clients.length; i++) {
-            Book book = books[i % books.length];
-            if (!book.isAvailable()) {
-                book.setAvailable(true);
-                reservations[i] = new Bron(clients[i], book, LocalDate.now(), LocalDate.now().plusDays(7));
-            }
-        };
-
-        for (Bron reservation : reservations) {
-            if (reservation != null) {
-                reservation.getBook().setAvailable(false);
-                System.out.println("Бронирование завершено для книги: " + reservation.getBook().getTitle());
-            }
-        }
+//        Bron[] reservations = new Bron[10];
+//
+//
+//        for (int i = 0; i < clients.length; i++) {
+//            Book book = books[i % books.length];
+//            if (!book.isAvailable()) {
+//                book.setAvailable(true);
+//                reservations[i] = new Bron(clients[i], book, LocalDate.now(), LocalDate.now().plusDays(7));
+//            }
+//        };
+//
+//        for (Bron reservation : reservations) {
+//            if (reservation != null) {
+//                reservation.getBook().setAvailable(false);
+//                System.out.println("Бронирование завершено для книги: " + reservation.getBook().getTitle());
+//            }
+//        }
     }
 }
